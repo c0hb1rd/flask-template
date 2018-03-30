bc0de08d6c623535a11b41e3a43a06f0f844259f
蔡培勤 <cohbird.zn@gmail.com>
2018-03-30 14:20:31 +0800

    去除无关代码

--- tornado.py
+++ tornado.py
@@ -1,7 +1,6 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
-from flask_socket import fk_socket
 
 
 http_server = HTTPServer(WSGIContainer(fk_socket.socket))
